5da90c157e47261f260c495274ce9803bc38813b
IvanVnucec <vnucec.ivan@gmail.com>
2019-12-21 09:59:51 +0100

    Update main.m

--- main.m
+++ main.m
@@ -9,19 +9,19 @@ y_inst  =  20;
 
 z_start =  -2;
 z_stop  =   2;
-z_inst  =  20;
+z_inst  =   20;
 
 % PARAMETRIZATION VARIABLE
 x = linspace(x_start, x_stop, x_inst);
 y = linspace(y_start, y_stop, y_inst);
 z = linspace(z_start, z_stop, z_inst);
 
-line = define_line([0, 0, 0]', [0, 0, 1]', 4, 2000);
+line = define_line([0, 0, 0]', [0, 0, 1]', 1, 2000);
 
 [curve, dl] = define_curve(line);
 
 [B_abs, B_x, B_y, B_z] = b_calc(x, y, z, curve, dl);
 % [B_abs, B_x, B_y, B_z] = b_calc_par(x, y, z, curve, dl);
 
-slice(X, Y, Z, log(B_abs), [0], [0], []);
+slice(x, y, z, log(B_abs), [0], [0], []);
 shading interp
