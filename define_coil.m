55ec5836b2c9c850c5ce69b6295593244786f794
IvanVnucec <vnucec.ivan@gmail.com>
2019-12-21 10:48:22 +0100

    Fixed coil function
    The function works again but now only it is drawing in z direction.

--- define_coil.m
+++ define_coil.m
@@ -1,36 +1,13 @@
-function r = define_coil(c_point, dir, R, N, l, inst)
+function r = define_coil(c_point, R, N, L, inst)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-dir = dir ./ norm(dir);
-
-% circle_inst = inst / N;
-% dl = (l / N) * dir;
-% 
-% r = [];
-% for i = 1:N
-%     r_i = define_cicrle(c_point, dir, R, circle_inst);
-%     r = cat(2, r, r_i);
-%     c_point = c_point + dl;
-% end
-
 t = linspace(0, 2*pi, inst);
 
-x = R*cos(N*t);
-y = R*sin(N*t);
-z = t * l/(2*pi);
+x = R * cos(N * t) + c_point(1);
+y = R * sin(N * t) + c_point(2);
+z = t * L / (2 * pi) + c_point(3);
 
 r = [x; y; z];
 
-n = [0, 0, 1]';
-
-alpha = vect_angle([0, 0, 1]', dir);
-beta  = vect_angle([0, 1, 0]', dir);
-gamma = vect_angle([1, 0, 0]', dir);
-
-r = vect_rot_zxy(r, alpha, beta, gamma) + c_point;
-end
-
-function angle = vect_angle(vect1, vect2)
-angle = acos(dot(vect1, vect2)/(norm(vect1)*norm(vect2)));
 end
